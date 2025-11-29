45c894d7d1ef1b1c268e75e1b67317f0cacc2c7d
NicoBocq <nicolas.fow@gmail.com>
2025-11-30 00:08:29 +0100

    remove deprecated

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
 })
